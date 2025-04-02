678241ff47dd8ee70a5a85fdbcdd4a78f23268c9
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2025-04-02 11:59:31 +0200

    Incluyo SDL y creo la ventana principal

--- main.cc
+++ main.cc
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main(){
 
-    std::cout << "Hello World" << std::endl;
+    SDL_Window * window_main = nullptr;
+    SDL_Renderer * render = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO); //initialize SDL library
+    SDL_CreateWindowAndRenderer(640*4, 480*4, 0, &window_main, &render); //Initialize window 
+    
 
     return 0;
 }
