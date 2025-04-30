2d7a6b3bf13edb1cd0edbb5b339c3cd7cabd5922
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2025-04-30 13:18:17 +0200

    Documented block.cc/.h

--- Figures/Block/Block.cc
+++ Figures/Block/Block.cc
@@ -2,36 +2,35 @@
 
 Block::Block(){
     this->block = {BSX, BSY, BLOCKLENGTH, BLOCKLENGTH};
-    this->inlineColor = WHITE;
-    this->outlineColor = WHITE;
-    this->blockX = 0;
-    this->blockY = 0;
+
     this->pixelX = BSX;
     this->pixelY = BSY;
+
+    this->blockX = 0;
+    this->blockY = 0;
+    
+
+    this->inlineColor = WHITE;
+    this->outlineColor = WHITE;
 }
 
 Block::~Block(){
     this->block = {0,0,0,0};
-    this->inlineColor = WHITE;
-    this->outlineColor = WHITE;
-    this->blockX = 0;
-    this->blockY = 0;
+
     this->pixelX = 0;
     this->pixelY = 0;
+
+    this->blockX = 0;
+    this->blockY = 0;
+
+    this->inlineColor = WHITE;
+    this->outlineColor = WHITE;
 }
+
 
 SDL_Rect Block::getBlock() const{
     return block;
 }
-
-SDL_Color Block::getInlineColor() const{
-    return inlineColor;
-}
-
-SDL_Color Block::getOutlineColor() const{
-    return inlineColor;
-}
-
 
 int Block::getPixelX() const{
     return pixelX;
@@ -49,12 +48,77 @@ int Block::getBlockY() const{
     return blockY;
 }
 
+SDL_Color Block::getInlineColor() const{
+    return inlineColor;
+}
+
+SDL_Color Block::getOutlineColor() const{
+    return outlineColor;
+}
+
+
+void Block::setPixelX(int pixelX){
+    this->pixelX = pixelX; 
+    blockX = convertPixeltoBlockX(); 
+    block.x = this->pixelX;
+}
+
+void Block::setPixelY(int pixelY){
+    this->pixelY = pixelY; 
+    blockY = convertPixelToBlockY(); 
+    block.y = this->pixelY;
+}
+
+void Block::setBlockX(int blockX){
+    this->blockX = blockX; 
+    pixelX = convertBlocktoPixelX();
+    block.x = pixelX;
+}
+
+void Block::setBlockY(int blockY){
+    this->blockY = blockY; 
+    pixelY = convertBlocktoPixelY(); 
+    block.y = pixelY;
+}
+
+void Block::setInlineColor(SDL_Color color){
+    inlineColor = color;
+}
+
+void Block::setOutlineColor(SDL_Color color){
+    outlineColor = color;
+}
+
+void Block::setPixelXDereferenced(int pixelX){
+    this->pixelX = pixelX; block.x = this->pixelX;
+}
+
+void Block::setPixelYDereferenced(int pixelY){
+    this->pixelY = pixelY; block.y = this->pixelY;
+}
+
+
+int Block::convertBlocktoPixelX(){
+    return blockX*BLOCKLENGTH + BSX;
+}
+
+int Block::convertBlocktoPixelY(){
+    return blockY*BLOCKLENGTH + BSY;
+}
+
+int Block::convertPixeltoBlockX(){
+    return (pixelX-BSX)/BLOCKLENGTH;
+}
+
+int Block::convertPixelToBlockY(){
+    return (pixelY-BSY)/BLOCKLENGTH;
+}
 
 void Block::renderBlock(){
     SDL_SetRenderDrawColor(mainScreen.getRender(), inlineColor.r, inlineColor.g, inlineColor.b, inlineColor.a);
-    SDL_RenderFillRect(mainScreen.getRender(), &this->block);
+    SDL_RenderFillRect(mainScreen.getRender(), &block);
     
     SDL_SetRenderDrawColor(mainScreen.getRender(), outlineColor.r, outlineColor.g, outlineColor.b, outlineColor.a);
-    SDL_RenderDrawRect(mainScreen.getRender(), &this->block);
+    SDL_RenderDrawRect(mainScreen.getRender(), &block);
 }
 
--- Figures/Block/Block.h
+++ Figures/Block/Block.h
@@ -5,7 +5,7 @@
 
 #include <SDL2/SDL.h>
 
-//This class is container for the "virtual" object of a block (I may also refer to it as a tile). 
+//This class is a container for the "virtual" object of a block (I may also refer to it as a tile). 
 //Each figure is composed of various blocks that are related to each other by the fact they all 
 //pertain to the same figure. There is no specific atribute that selects which figure it is 
 //associated to. The way to know where a block belongs to is by searching with the specific coordianates 
@@ -22,6 +22,7 @@
 class Block{
 
     private:
+
         //The SDL_Rect instance that visually represents the block
         SDL_Rect block;
         
@@ -71,20 +72,6 @@ class Block{
         //Getter
         //
         //RETURNS:
-        //The SDL_Color instance that references
-        //the inside color of the block
-        SDL_Color getInlineColor() const;
-
-        //Getter
-        //
-        //RETURNS:
-        //The SDL_Color instance that references
-        //the outside color of the block
-        SDL_Color getOutlineColor() const;
-
-        //Getter
-        //
-        //RETURNS:
         //An integer representing the position X
         //in pixels of the block
         int getPixelX() const;
@@ -95,40 +82,143 @@ class Block{
         //An integer representing the position Y
         //in pixels of the block
         int getPixelY() const;
-
+        
         //Getter
         //
         //RETURNS:
         //An integer representing the position X
         //in tiles of the block. See doc. 4 + info.
         int getBlockX() const;
-
+        
         //Getter
         //
         //RETURNS:
         //An integer representing the position Y
         //in tiles of the block. See doc. 4 + info.
         int getBlockY() const;
+        
+        //Getter
+        //
+        //RETURNS:
+        //The SDL_Color instance that references
+        //the inside color of the block
+        SDL_Color getInlineColor() const;
 
+        //Getter
+        //
+        //RETURNS:
+        //The SDL_Color instance that references
+        //the outside color of the block
+        SDL_Color getOutlineColor() const;
+        
 
         //SETTERS
+ 
+        //Setter
+        //Also sets de tile X coordinate (both the atribute blockX and the SDL_Rect.x).
+        //Calls convertPixelToBlockX()
+        //
+        //Returns:
+        //Void
+        void setPixelX(int pixelX);
+        
+        //Setter
+        //Also sets de tile Y coordinate (both the atribute blockY and the SDL_Rect.y)
+        //Calls convertPixelToBlockY()
+        //
+        //Returns:
+        //Void
+        void setPixelY(int pixelY);
 
-        void setBlockColor(SDL_Color color){this->inlineColor = color;};
-        void setOutlineColor(SDL_Color color){this->outlineColor = color;};
-        void setPixelX(int pixelX){this->pixelX = pixelX; this->blockX = this->convertPixeltoBLockX(); this->block.x = this->pixelX;};
-        void setPixelY(int pixelY){this->pixelY = pixelY; this->blockY = this->convertPixelToBlockY(); this->block.y = this->pixelY;};
-        void setBlockX(int blockX){this->blockX = blockX; this->pixelX = this->convertBlocktoPixelX(); this->block.x = this->pixelX;};
-        void setBlockY(int blockY){this->blockY = blockY; this->pixelY = this->convertBlocktoPixelY(); this->block.y = this->pixelY;};
-        void setPixelXDereferenced(int pixelX){this->pixelX = pixelX; this->block.x = this->pixelX;};
-        void setPixelYDereferenced(int pixelY){this->pixelY = pixelY; this->block.y = this->pixelY;};
+        //Setter
+        //Also sets de pixel X coordinate (pixelX). Calls convertBlocktoPixelX()
+        //
+        //Returns:
+        //Void
+        void setBlockX(int blockX);
+        
+        //Setter
+        //Also sets de tile Y coordinate (pixelY). Calls convertBlockToPixelY()
+        //
+        //Returns:
+        //Void
+        void setBlockY(int blockY);
+        
+        //Setter
+        //
+        //Returns:
+        //Void
+        void setInlineColor(SDL_Color color);
+
+        //Setter
+        //
+        //Returns:
+        //Void
+        void setOutlineColor(SDL_Color color);
+
+        //Setter
+        //Acts the same as setPixelX but avoids updating blockX. Also avoids calling
+        //convertPixelToBlockX()
+        //
+        //Returns:
+        //Void
+        void setPixelXDereferenced(int pixelX);
+        
+        //Setter
+        //Acts the same as setPixelY but avoids updating blockY. Also avoids calling
+        //convertPixelToBlockY()
+        //
+        //Returns:
+        //Void
+        void setPixelYDereferenced(int pixelY);
 
 
-        int convertBlocktoPixelX(){return this->blockX*BLOCKLENGTH + BSX;};
-        int convertBlocktoPixelY(){return this->blockY*BLOCKLENGTH + BSY;};
+        //MISCELANEOUS
 
-        int convertPixeltoBLockX(){return (this->pixelX-BSX)/BLOCKLENGTH;};
-        int convertPixelToBlockY(){return (this->pixelY-BSY)/BLOCKLENGTH;};
+        //This method gives the equivalent in pxels of the current tile X position
+        //It is a formula to traduce from one coordinate to another. Take into account
+        //that the tile coordinate has it's zero on the BSX,BSY points, not on the upper
+        //left corner of the screen.
+        //
+        //RETURNS:
+        //An integer repreenting the poisition X in pixels of the X tile coordinate
+        int convertBlocktoPixelX();
 
+        //This method gives the equivalent in pxels of the current tile Y position
+        //It is a formula to traduce from one coordinate to another. Take into account
+        //that the tile coordinate has it's zero on the BSX,BSY points, not on the upper
+        //left corner of the screen.
+        //
+        //RETURNS:
+        //An integer representing the poisition Y in pixels of the Y tile coordinate
+        int convertBlocktoPixelY();
+
+        //This method gives the equivalent in tiles of the current pixel X position
+        //It is a formula to traduce from one coordinate to another. Take into account
+        //that the tile coordinate has it's zero on the BSX,BSY points, not on the upper
+        //left corner of the screen.
+        //
+        //RETURNS:
+        //An integer repreenting the poisition X in tiles of the pixel X coordinate
+        int convertPixeltoBlockX();
+
+        //This method gives the equivalent in tiles of the current pixel Y position
+        //It is a formula to traduce from one coordinate to another. Take into account
+        //that the tile coordinate has it's zero on the BSX,BSY points, not on the upper
+        //left corner of the screen.
+        //
+        //RETURNS:
+        //An integer repreenting the poisition Y in tiles of the pixel Y coordinate
+        int convertPixelToBlockY();
+
+
+        //This method renders the block to the screen. Whenever a figure is rendered, this method
+        //will be called consecutively on all the blocks of the figure. This is to say, any method
+        //that attempts to render a figure, is internally calling this method for each of it's blocks.
+        //Keep in mind this method doesn't show the block directly, it only renders it.
+        //
+        //RETURNS:
+        //Void
         void renderBlock();
 
 };
